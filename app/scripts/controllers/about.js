b7e1849a1ea030993a4fe156956f5f4458556f51
Ryan <ryan.hd.ren@gamil.com>
2014-10-07 16:27:14 +0800

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name financeVisoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the financeVisoApp
+ */
+angular.module('financeVisoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
